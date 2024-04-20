e1eec41d8aa8a343cbb164fb7070ddabc55bef61
chikephils <chikephils@gmail.com>
2024-04-20 10:12:02 +0100

    updated

--- server/app.js
+++ server/app.js
@@ -4,6 +4,8 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 app.use(
   cors({
     origin: "https://rentals-blond.vercel.app/",
@@ -11,9 +13,6 @@ app.use(
     credentials: true,
   })
 );
-
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 
 app.use(express.json());
 app.use(cookieParser());
