dd1baefd3a3f537c1faa99babb3dfa0faed633b0
chikephils <chikephils@gmail.com>
2024-04-25 14:31:29 +0100

    updated

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.static("public"));
 
-app.use("/test", (req, res) => {
+app.use("/", (req, res) => {
   res.send("Hello world! Your Server is Running...");
 });
 
