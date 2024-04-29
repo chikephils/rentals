3557b5f8d81c1bf2061d66d7779b6d490079dc2c
chikephils <chikephils@gmail.com>
2024-04-29 15:26:57 +0100

    updated

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -106,17 +106,17 @@ const CreateListing = () => {
       listingForm.append("title", formDescription.title);
       listingForm.append("description", formDescription.description);
       listingForm.append("price", formDescription.price);
-  
+
       photos.forEach((photo) => {
         listingForm.append("listingPhotos", photo);
       });
-  
+      console.log(...listingForm);
       const response = await axios.post(
         `${server}/listing/create`,
         listingForm,
         {
           headers: {
-            "Content-Type": "multipart/form-data", 
+            "Content-Type": "multipart/form-data",
           },
         }
       );
@@ -130,7 +130,6 @@ const CreateListing = () => {
       setLoading(false);
     }
   };
-  
 
   return (
     <>
