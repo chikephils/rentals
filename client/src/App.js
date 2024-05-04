5f91ff04c32cbc8f9d5b0ed5cf57004fdcae09b8
chikephils <chikephils@gmail.com>
2024-05-04 01:08:53 +0100

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -20,7 +20,7 @@ import "react-toastify/dist/ReactToastify.css";
 import { getAllListings } from "./redux/listing";
 
 function App() {
-  const isAuth = Boolean(useSelector((state) => state.user?.token));
+  const isAuth = Boolean(useSelector((state) => state.user?.user?.token));
   const dispatch = useDispatch();
 
   useEffect(() => {
